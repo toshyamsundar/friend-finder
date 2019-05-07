0f6272810e5587afae37c5872ddccb680290523e
toshyamsundar <toshyamsundar@yahoo.com>
2019-05-07 18:53:11 -0400

    Created placeholder endpoint

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ let PORT = process.env.PORT || 3000;
 
 let app = express();
 
+app.get("/", (request, response) => {
+  response.send("Just testing the app");
+});
+
 app.listen(PORT, () => {
   console.log(`Express server listening on PORT ${PORT}`);
 });
