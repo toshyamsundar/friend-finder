e29c476fcdcd9cf1c3f8db0587cf085ed823ef75
toshyamsundar <toshyamsundar@yahoo.com>
2019-05-07 19:10:51 -0400

    Moved css and images to public & added path package

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 let express = require("express");
+let path = require("path");
 
 let PORT = process.env.PORT || 3000;
 
 let app = express();
 
 app.get("/", (request, response) => {
-  response.send("Just testing the app");
+  // let responseHTMLFile = path.join(__dirname);
+  response.sendFile(__dirname);
 });
 
 app.listen(PORT, () => {
